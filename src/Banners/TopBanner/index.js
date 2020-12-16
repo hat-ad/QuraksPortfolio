ceba0d8e5b0c964e1808cc5957beeec68ef7a97e
hat-ad <dasashis672@gmail.com>
2020-12-16 18:58:41 +0530

    changes in navbar and some style fixes

--- src/Banners/TopBanner/index.js
+++ src/Banners/TopBanner/index.js
@@ -15,19 +15,19 @@ function index() {
     <>
       <div className="container" id="home">
         <NavBar />
-        <Grid container xs={12} justify="center" alignItems="center" item>
-          <Grid item lg={6} sm={6}>
+        <Grid container xs={12} lg={12} sm={12} md={12} justify="center" alignItems="center" item>
+          <Grid item lg={6} sm={6} md={6} xs={6}>
             <div className="banner-img-container">
               <img src={Banner} alt="Banner" className="banner-img" />
             </div>
           </Grid>
-          <Grid item lg={6} sm={6}>
+          <Grid item lg={6} sm={6}  md={6} xs={6}>
             <div className="banner-text-container">
               <span className="banner-title">Quarks Technology</span>
-              <ul>
-                <li className="banner-text">lorem ipsum</li>
-                <li className="banner-text">lorem ipsum</li>
-                <li className="banner-text">lorem ipsum</li>
+              <ul className="banner-text-ul">
+                <li className="banner-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit,</li>
+                <li className="banner-text">Sit amet consectetur adipiscing elit. Tincidunt dui ut</li>
+                <li className="banner-text">Commodo nulla facilisi nullam vehicula ipsum a. </li>
               </ul>
             </div>
           </Grid>

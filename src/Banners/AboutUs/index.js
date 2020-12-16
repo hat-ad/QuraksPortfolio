56ea700178b2eac0ca5ab8e5b07abd08306b4e3f
Souvik-Bhattacharyya <souvikbhattacharyya2001@gmail.com>
2020-12-16 05:33:10 -0800

    about us

--- src/Banners/AboutUs/index.js
+++ src/Banners/AboutUs/index.js
@@ -1,55 +1,60 @@
+/* eslint-disable jsx-a11y/alt-text */
 import React from "react";
 import "./style.css";
 
 function AboutUs() {
   return (
     <div>
-      <div className="services" id="about_us">
-        {/* <p className="text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc mattis sagittis varius nec tempus
-                  facilisis
-                  accumsan dui. Aliquam non nibh nunc morbi ac, sagittis. Eget eu, aliquet sit mattis sed. Eu
-                  adipiscing
-                  eget est morbi etiam id donec. Scelerisque consequat semper aliquet imperdiet est quis adipiscing.
+      <div className="aboutus" id="about_us">
+        <h2 className="about-us-heading">About Us</h2>
+        <hr className="about-us-hr" />
+        <p className="about-us-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc mattis sagittis varius nec tempus
+        facilisis
+        accumsan dui. Aliquam non nibh nunc morbi ac, sagittis. Eget eu, aliquet sit mattis sed. Eu
+        adipiscing
+        eget est morbi etiam id donec. Scelerisque consequat semper aliquet imperdiet est quis adipiscing.
                     </p>
-                    <div className="Bx">
-                        <div className="serviceBx">
-                            <div>
-                                <img src="Assets/undraw_male_avatar_323b.svg"/>
-                                <h2 className="card-head">Souvik Bhattacharyya</h2>
-                                <h5>Designer</h5>
-                                <p>
-                          Consectetur adipisicing elit, seddosdoe eiusmod tempor incididunt utore etstes dolore magna
-                          aliqua.
+        <div className="about-us-container">
+          <div className="about-us-card">
+            <div>
+              <img src={require("../../Assets/Svg/undraw_male_avatar_323b.svg").default} />
+              <h2 className="card-head">Souvik Bhattacharyya</h2>
+              <h5 className="card-head-catagory">Designer</h5>
+              <p className="card-head-details">
+                Consectetur adipisicing elit, seddosdoe eiusmod tempor incididunt utore etstes dolore magna
+                aliqua.
                                 </p>
-                        </div>
-                        <div className="serviceBx">
-                            <div>
-                                <img src="Assets/undraw_male_avatar_323b.svg" />
-                                <h2 className="card-head2">Subho Basak</h2>
-                                <h5>Designer</h5>
-                                <p>
-                          Consectetur adipisicing elit, seddosdoe eiusmod tempor incididunt utore etstes dolore magna
-                          aliqua.
+            </div>
+            </div>
+            <div className="about-us-card">
+              <div>
+              <img src={require("../../Assets/Svg/undraw_male_avatar_323b.svg").default} />
+                <h2 className="card-head2">Subho Basak</h2>
+                <h5 className="card-head-catagory">Designer</h5>
+                
+                <p className="card-head-details">
+                  Consectetur adipisicing elit, seddosdoe eiusmod tempor incididunt utore etstes dolore magna
+                  aliqua.
                                 </p>
-                            </div>
-                        </div>
-                        <div className="serviceBx">
-                            <div>
-                                <img src="Assets/undraw_male_avatar_323b.svg" />
-                                <h2 className="card-head2">Ashis Kr Das</h2>
-                                <h5>Designer</h5>
-                                <p>
-                          Consectetur adipisicing elit, seddosdoe eiusmod tempor incididunt utore etstes dolore magna
-                          aliqua.
+
+              </div>
+            </div>
+            <div className="about-us-card">
+              <div>
+              <img src={require("../../Assets/Svg/undraw_male_avatar_323b.svg").default} />
+                <h2 className="card-head2">Ashis Kr Das</h2>
+                <h5 className="card-head-catagory">Designer</h5>
+                <p className="card-head-details">
+                  Consectetur adipisicing elit, seddosdoe eiusmod tempor incididunt utore etstes dolore magna
+                  aliqua.
                                 </p>
-                            </div>
-                        </div>
-                    </div>
-                    </div> */}
+              </div>
+            </div>
+          </div>
       </div>
       <div className="gallery">
-        <h2 className="heading">Our Projects</h2>
-        <hr />
+        <h2 className="gallery-heading">Our Projects</h2>
+        <hr className="gallery-hr" />
         <img
           src={require("../../Assets/Svg/Carosel.svg").default}
           alt="aboutus-banner"

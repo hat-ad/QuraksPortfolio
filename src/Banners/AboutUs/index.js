c904bf2bb5f14d360a2e115a77bef8aae0a211f1
hat-ad <dasashis672@gmail.com>
2020-12-19 09:45:21 +0530

    redesigned navbar added carousel

--- src/Banners/AboutUs/index.js
+++ src/Banners/AboutUs/index.js
@@ -2,63 +2,84 @@
 import React from "react";
 import "./style.css";
 
+import { Carousel } from "3d-react-carousal";
+
 function AboutUs() {
+  let slides = [
+    <img src="https://picsum.photos/800/300/?random" alt="1" />,
+    <img src="https://picsum.photos/800/301/?random" alt="2" />,
+    <img src="https://picsum.photos/800/302/?random" alt="3" />,
+    <img src="https://picsum.photos/800/303/?random" alt="4" />,
+    <img src="https://picsum.photos/800/304/?random" alt="5" />,
+  ];
   return (
     <div>
       <div className="aboutus" id="about_us">
         <h2 className="about-us-heading">About Us</h2>
         <hr className="about-us-hr" />
-        <p className="about-us-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc mattis sagittis varius nec tempus
-        facilisis
-        accumsan dui. Aliquam non nibh nunc morbi ac, sagittis. Eget eu, aliquet sit mattis sed. Eu
-        adipiscing
-        eget est morbi etiam id donec. Scelerisque consequat semper aliquet imperdiet est quis adipiscing.
-                    </p>
+        <p className="about-us-text">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc mattis
+          sagittis varius nec tempus facilisis accumsan dui. Aliquam non nibh
+          nunc morbi ac, sagittis. Eget eu, aliquet sit mattis sed. Eu
+          adipiscing eget est morbi etiam id donec. Scelerisque consequat semper
+          aliquet imperdiet est quis adipiscing.
+        </p>
         <div className="about-us-container">
           <div className="about-us-card">
             <div>
-              <img src={require("../../Assets/Svg/undraw_male_avatar_323b.svg").default} />
+              <img
+                src={
+                  require("../../Assets/Svg/undraw_male_avatar_323b.svg")
+                    .default
+                }
+              />
               <h2 className="card-head">Souvik Bhattacharyya</h2>
               <h5 className="card-head-catagory">Designer</h5>
               <p className="card-head-details">
-                Consectetur adipisicing elit, seddosdoe eiusmod tempor incididunt utore etstes dolore magna
-                aliqua.
-                                </p>
-            </div>
-            </div>
-            <div className="about-us-card">
-              <div>
-              <img src={require("../../Assets/Svg/undraw_male_avatar_323b.svg").default} />
-                <h2 className="card-head2">Subho Basak</h2>
-                <h5 className="card-head-catagory">Designer</h5>
-                
-                <p className="card-head-details">
-                  Consectetur adipisicing elit, seddosdoe eiusmod tempor incididunt utore etstes dolore magna
-                  aliqua.
-                                </p>
-
-              </div>
-            </div>
-            <div className="about-us-card">
-              <div>
-              <img src={require("../../Assets/Svg/undraw_male_avatar_323b.svg").default} />
-                <h2 className="card-head2">Ashis Kr Das</h2>
-                <h5 className="card-head-catagory">Designer</h5>
-                <p className="card-head-details">
-                  Consectetur adipisicing elit, seddosdoe eiusmod tempor incididunt utore etstes dolore magna
-                  aliqua.
-                                </p>
-              </div>
+                Consectetur adipisicing elit, seddosdoe eiusmod tempor
+                incididunt utore etstes dolore magna aliqua.
+              </p>
             </div>
           </div>
+          <div className="about-us-card">
+            <div>
+              <img
+                src={
+                  require("../../Assets/Svg/undraw_male_avatar_323b.svg")
+                    .default
+                }
+              />
+              <h2 className="card-head2">Subho Basak</h2>
+              <h5 className="card-head-catagory">Designer</h5>
+
+              <p className="card-head-details">
+                Consectetur adipisicing elit, seddosdoe eiusmod tempor
+                incididunt utore etstes dolore magna aliqua.
+              </p>
+            </div>
+          </div>
+          <div className="about-us-card">
+            <div>
+              <img
+                src={
+                  require("../../Assets/Svg/undraw_male_avatar_323b.svg")
+                    .default
+                }
+              />
+              <h2 className="card-head2">Ashis Kr Das</h2>
+              <h5 className="card-head-catagory">Designer</h5>
+              <p className="card-head-details">
+                Consectetur adipisicing elit, seddosdoe eiusmod tempor
+                incididunt utore etstes dolore magna aliqua.
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
       <div className="gallery">
         <h2 className="gallery-heading">Our Projects</h2>
         <hr className="gallery-hr" />
-        <img
-          src={require("../../Assets/Svg/Carosel.svg").default}
-          alt="aboutus-banner"
-        />
+        <Carousel slides={slides} autoplay={true} interval={3000} />
       </div>
     </div>
   );
